a2a68303f1906e4c49d02f153d175d4d7144f6b6
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-03-11 20:15:55 -0800

    anyway

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,14 @@
+const User = require("../models/User");
 const router = require("express").Router();
-router.get('',(req, res)=>{
-  res.send("hey its user route")
-})
+const bcrypt = require("bcrypt");
 
+//update user
+router.put("/:id", async (req, res) => {
+  res.send("hey its user route");
+});
 
+//delete user
+//get a user
+//follow a user
+/// unfollow a user
 module.exports = router;
